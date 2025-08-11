6f94046f6c8c8370a18507df1355df600ffbbeda
Hirakida Jungo <jmailmag@gmail.com>
2025-08-11 09:41:37 +0900

    計算ボタンを押したら計算結果にスクロール

--- calculate_conditions.py
+++ calculate_conditions.py
@@ -18,20 +18,43 @@ def calculate_conditions(scores, oya, tsumibo, kyotaku):
     is_parent = (oya == me)
     role_str = "親" if is_parent else "子"
 
+    # 計算詳細を格納する辞書
+    calculation_details = {
+        'current_scores': scores.copy(),
+        'leader': leader,
+        'leader_score': leader_score,
+        'my_score': my_score,
+        'top_diff': top_diff,
+        'kyotaku_points': kyotaku_points,
+        'tsumibo_points': tsumibo_points,
+        'tsumo_tsumibo_points': tsumo_tsumibo_points,
+        'is_parent': is_parent,
+        'role_str': role_str
+    }
+
     results = []
 
     # Direct Ron (from leader) - 直撃時は点差を半分に
     need_direct = top_diff - kyotaku_points - tsumibo_points
     need_direct = max(0, need_direct)
+    need_direct_original = need_direct
     need_direct = need_direct / 2
     need_direct = ceil100(need_direct)
     rev_direct = reverse_lookup(need_direct, 'ron', is_parent)
     results.append({
         '条件': f'直撃ロン（{leader}）（{role_str}）',
         'need_points': need_direct,
+        'need_points_original': need_direct_original,
         'rank': rev_direct['rank'],
         'display': rev_direct['points'],
-        'is_direct': True
+        'is_direct': True,
+        'calculation_steps': {
+            'top_diff': top_diff,
+            'minus_kyotaku': top_diff - kyotaku_points,
+            'minus_tsumibo': top_diff - kyotaku_points - tsumibo_points,
+            'divided_by_2': need_direct_original / 2,
+            'final_points': need_direct
+        }
     })
 
     # Other Ron (no name)
@@ -44,7 +67,13 @@ def calculate_conditions(scores, oya, tsumibo, kyotaku):
         'need_points': need_other,
         'rank': rev_other['rank'],
         'display': rev_other['points'],
-        'is_direct': False
+        'is_direct': False,
+        'calculation_steps': {
+            'top_diff': top_diff,
+            'minus_kyotaku': top_diff - kyotaku_points,
+            'minus_tsumibo': top_diff - kyotaku_points - tsumibo_points,
+            'final_points': need_other
+        }
     })
 
     # Tsumo
@@ -58,9 +87,18 @@ def calculate_conditions(scores, oya, tsumibo, kyotaku):
         results.append({
             '条件': f'ツモ（{role_str}）',
             'need_points': per_person,
+            'total_needed': total_needed,
             'rank': rev_t['rank'],
             'display': rev_t['points'],
-            'is_direct': False
+            'is_direct': False,
+            'calculation_steps': {
+                'top_diff': top_diff,
+                'minus_kyotaku': top_diff - kyotaku_points,
+                'minus_tsumo_tsumibo': top_diff - kyotaku_points - tsumo_tsumibo_points,
+                'divided_by_3': total_needed / 3.0,
+                'ceiled': math.ceil(total_needed / 3.0),
+                'final_points': per_person
+            }
         })
     else:
         # 子ツモ：親から2倍、子から1倍の点数をもらう
@@ -71,9 +109,23 @@ def calculate_conditions(scores, oya, tsumibo, kyotaku):
         results.append({
             '条件': f'ツモ（{role_str}）',
             'need_points': per_child,
+            'total_needed': total_needed,
             'rank': rev_t['rank'],
             'display': rev_t['points'],
-            'is_direct': False
+            'is_direct': False,
+            'calculation_steps': {
+                'top_diff': top_diff,
+                'minus_kyotaku': top_diff - kyotaku_points,
+                'minus_tsumo_tsumibo': top_diff - kyotaku_points - tsumo_tsumibo_points,
+                'divided_by_3': total_needed / 3.0,
+                'ceiled': math.ceil(total_needed / 3.0),
+                'final_points': per_child
+            }
         })
 
-    return {'top_diff': top_diff, 'leader': leader, 'results': results}
+    return {
+        'top_diff': top_diff, 
+        'leader': leader, 
+        'results': results,
+        'calculation_details': calculation_details
+    }
